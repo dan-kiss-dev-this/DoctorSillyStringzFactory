a3345ca12550d85b76ae3917cda7badff30e47a9
Dan Kiss <flowfast47@gmail.com>
2023-08-04 15:01:02 -0700

    add migration for details column

---
+++ Factory/Migrations/20230804215445_AddDetails.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class AddDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Details",
+                table: "Machines",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Details",
+                table: "Engineers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Details",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "Details",
+                table: "Engineers");
+        }
+    }
+}
